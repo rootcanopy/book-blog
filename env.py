384e6b8d5377b0e9cd06488be757047d1d9c2dda
rootcanopy <dave.trees201@gmail.com>
2020-04-14 23:05:52 +0200

    setting up enviroment variables

---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ["MONGO_URI"] = "mongodb+srv://rootcanopy:Di1LJQyxmTBKIvCj@mycluster-t6yqh.mongodb.net/awesome-reads?retryWrites=true&w=majority"
+
+os.environ["PORT"] = "5000"
