b113b13bc6e31adc60ae9e2eef90e9e7c1b9c904
rootcanopy <dave.trees201@gmail.com>
2020-04-16 11:26:18 +0200

    everything is good atm, except the login page isint rendering onto home.html

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import os
 import env
-from forms import RegistrationForm, LoginForm
+from forms import RegistrationForm, LoginForm, UpdateAccountForm
 from flask import Flask, render_template, redirect, url_for, flash, request, session
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from datetime import datetime
+from flask_user import current_user, login_required, UserMixin, login_required
+from flask_user.forms import EditUserProfileForm
 import bcrypt
-from flask_login import login_user, logout_user, login_required
 
 
 app = Flask(__name__)
@@ -58,15 +59,15 @@ def register():
     form = RegistrationForm(request.form)
     if form.validate_on_submit():
         
-        users = mongo.db.Users
+        user = mongo.db.users
         existing_user = users.find_one({'username': request.form['username']})
         
         if existing_user is None:
             # HASHED PW AND INSERT TO COLL
             hashed_pw = bcrypt.hashpw(request.form['password'].encode('utf-8'), bcrypt.gensalt())
-            users.insert_one({'username': request.form['username'],
-                                'email': request.form['email'],
-                                'password': hashed_pw})
+            mongo.db.users.insert({'username': request.form['username'],
+                                   'email': request.form['email'],
+                                   'password': hashed_pw})
             session['username'] = request.form['username']
             # IF REGISTER IS SUCCESSFUL
             flash(f'Account created for {form.username.data}.. now just to login', 'success')
@@ -74,40 +75,44 @@ def register():
 
         else:
             flash(f'Registration Failed. Check yo\'self, please try again!', 'danger')
-        return render_template('register.html', title='Register', form=form)
-
-    return render_template('register.html', title = 'Register', form=form)
+    return render_template('register.html', title='Register', form=form)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     # LOGIN HANDLER
+    form = LoginForm()
     if session.get('logged_in'):
         if session['logged_in'] is True:
             return redirect(url_for('home', title='Home'))
      
-    form = LoginForm()
-    user = mongo.db.Users.find_one({'email': form.email.data})
+        user = mongo.db.users.find_one({'email': form.email.data})
 
-    if form.validate_on_submit():
-        users = mongo.db.users
-        db_user = users.find_one({'email': request.form['email'] })
+        if form.validate_on_submit():
+            user = mongo.db.users
+            db_user = users.find_one({'email': request.form['email'] })
 
-        if db_user:
-            if bcrypt.hashpw(request.form['password'].encode('utf-8'),
-                db_user['password']) == db_user['password']:
-                session['email'] = request.form['email']
-                session['logged_in'] = True
-        return redirect(url_for('home', title='Signed In', form=form))
-    else:
-        flash('Login Failed.. Please Check Yourself!', 'danger')
+
+            if db_user:
+                if bcrypt.hashpw(request.form['password'].encode('utf-8'),
+                    db_user['password']) == db_user['password']:
+                    session['email'] = request.form['email']
+                    session['logged_in'] = True
+                    session['user_id'] = user_live
+                    session['usertype'] = user['type']
+            
+                return redirect(url_for('home', title='Account'))
+        else:
+            flash('Login Failed.. Please Check Yourself!', 'danger')
     return render_template('login.html', title = 'Log In', form=form)
 
 
 @app.route('/account')
+@login_required
 def account():
-    image_file = url_for('static', filename='images/')
-    return render_template('account.html', title='My Account')
+    form = UpdateAccountForm()
+    image_file = url_for('static', filename='images/ + current_user.image_file')
+    return render_template('account.html', title='My Account', image_file=image_file, form=form)
 
 
 @app.route('/logout')
--- forms.py
+++ forms.py
@@ -1,5 +1,6 @@
 import os
 from flask_wtf import FlaskForm
+from flask_user import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, validators
 from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
 
@@ -11,20 +12,41 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password', message='Passwords must match.')])
     submit = SubmitField('Sign Up')
 
-    """
-    def validate_username(self, username):
-        user = mongo.db.Users.find_one(username=username.data)
-        if user is not None:
-            raise ValidationError('Username is already taken.')
 
-    def validate_email(self, username):
-        user = mongo.db.Users.find_one(email=email.data)
-        if user is not None:
-            raise ValidationError('Email is already taken.')
-    """
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = mongo.db.users.find(username=username.data)
+            if user is not None:
+                raise ValidationError('Username is already taken.')
+
+    def validate_email(self, email):
+        if username.data != current_user.username:
+            user = mongo.db.users.find(email=email.data)
+            if user is not None:
+                raise ValidationError('Email is already taken.')
+
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Update')
+
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = mongo.db.users.find(username=username.data)
+            if user is not None:
+                raise ValidationError('Username is already taken.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = mongo.db.users.find(email=email.data)
+            if user is not None:
+                raise ValidationError('Email is already taken.')
